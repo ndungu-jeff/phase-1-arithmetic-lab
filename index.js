c991dc03f1208539a1bc101b0c1cc6ebf533dd6e
jeff ndungu <ndungujeff.ke@gmail.com>
2022-11-23 14:23:20 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+let multiply = (num1*num2)
+
+let random = Number.isInteger(Math.random() *10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+ let mod = (num3%num4);
+
+ let max = Math.max(1,6,8,9,20)
+ 
